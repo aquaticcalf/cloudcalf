86af95e8fe10786a54c86aeb88cc84333d66a7e8
sam <aquaticcalf@proton.me>
2026-06-17 20:30:59 +0530

    vite plugin

--- main/index.ts
+++ main/index.ts
@@ -1,3 +1,23 @@
-export function add(a: number, b: number): number {
-  return a + b
+import type { Plugin, ResolvedConfig } from "vite"
+
+export interface cloudcalfPluginOptions {
+  test?: string
 }
+
+export function cloudcalf(options: cloudcalfPluginOptions = {}): Plugin {
+  let config: ResolvedConfig
+
+  return {
+    name: "cloudcalf",
+
+    configResolved(resolvedConfig) {
+      config = resolvedConfig
+      console.log("[cloudcalf] ", options.test)
+      if (config.command === "build") {
+        config.log("[cloudcalf] build test")
+      }
+    },
+  }
+}
+
+export default cloudcalf
